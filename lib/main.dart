b10102a5b19c3c76339fb0f5428a85971d637def
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-19 11:00:44 +0530

    project structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import 'dependency_injector.dart';
+import 'src/config/app_routes.dart';
 import 'src/config/widget_keys.dart';
 import 'src/ui/pages/splash_screen.dart';
 
@@ -31,6 +32,8 @@ class MyApp extends StatelessWidget {
           home: const SplashScreen(),
           navigatorKey: WidgetKeys.mainNavKey,
           scaffoldMessengerKey: WidgetKeys.mainScaffoldMessengerKey,
+          initialRoute: RouteNames.splashScreen,
+          onGenerateRoute: (settings) => RouteConfig.generateRoute(settings),
         );
       }
     );
