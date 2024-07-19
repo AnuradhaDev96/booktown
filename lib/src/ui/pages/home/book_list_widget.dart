e8318cd9daef92a862103ab3cbd13bb18333c966
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-19 15:55:30 +0530

    B02 - Fetch new books

--- lib/src/ui/pages/home/book_list_widget.dart
+++ lib/src/ui/pages/home/book_list_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../bloc/cubits/fetch_books_cubit.dart';
 import '../../../bloc/states/fetch_books_state.dart';
+import '../../../models/book.dart';
 
 class BookListWidget extends StatelessWidget {
   BookListWidget({super.key});
@@ -24,16 +25,70 @@ class BookListWidget extends StatelessWidget {
           if (state is LoadingBooksState) {
             return const Text("Implement shimmer");
           } else if (state is BookListLoadedState) {
-            return ListView.builder(
-              itemBuilder: (context, index) => SizedBox(height: 80, child: Text(state.newBooks[index].title)),
-              itemCount: state.newBooks.length,
-            );
+            return PaginatedBookListView(newBooks: state.newBooks);
           } else if (state is BookErrorState) {
             return Text(state.message);
           } else {
             return const SizedBox.shrink();
           }
         },
+      ),
+    );
+  }
+}
+
+class PaginatedBookListView extends StatefulWidget {
+  const PaginatedBookListView({super.key, required this.newBooks});
+
+  final List<BookDto> newBooks;
+
+  @override
+  State<PaginatedBookListView> createState() => _PaginatedBookListViewState();
+}
+
+class _PaginatedBookListViewState extends State<PaginatedBookListView> {
+  final ScrollController _scrollController = ScrollController();
+
+  int _currentPage = 0;
+  final int _pageSize = 6;
+
+  final List<BookDto> _paginatedList = <BookDto>[];
+
+  void _loadNextPage() {
+    final int startIndex = _currentPage * _pageSize;
+    final int endIndex = startIndex + _pageSize;
+
+    if (startIndex < widget.newBooks.length) {
+      setState(() {
+        _paginatedList.addAll(widget.newBooks.sublist(
+          startIndex,
+          endIndex > widget.newBooks.length ? widget.newBooks.length : endIndex,
+        ));
+        _currentPage++;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<ScrollNotification>(
+      onNotification: (notification) {
+        if (notification is ScrollEndNotification) {
+          // User has reached the end of the list. Load more data
+          _loadNextPage();
+        }
+        return false;
+      },
+      child: ListView.builder(
+        controller: _scrollController,
+        itemBuilder: (context, index) => SizedBox(height: 140, child: Text("$index\n${_paginatedList[index].title}")),
+        itemCount: _paginatedList.length,
       ),
     );
   }
