bbf4480046d5b61b7e28448aa8e01aa61541dbb9
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-19 17:53:12 +0530

    B02 - Search books

---
+++ lib/src/bloc/cubits/search_book_result_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../services/repositories/book_repository.dart';
+import '../states/search_book_results_state.dart';
+
+class SearchBookResultCubit extends Cubit<SearchBookResultsState> {
+  SearchBookResultCubit() : super(LoadingSearchState());
+
+  void fetchNewBooks(String query, int page) {
+    GetIt.instance<BookRepository>().searchBooksByTitle(query, page.toString()).then((result) {
+      result.fold(
+            (newPage) {
+
+            },
+            (message) => emit(SearchBookErrorState(message: message)),
+      );
+    });
+  }
+}
---
+++ lib/src/bloc/states/search_book_results_state.dart
@@ -0,0 +1,17 @@
+import '../../models/book.dart';
+
+class SearchBookResultsState {}
+
+class LoadingSearchState extends SearchBookResultsState {}
+
+class SearchBookResultPageState extends SearchBookResultsState {
+  final BookPageResponseDto pageResponse;
+
+  SearchBookResultPageState({required this.pageResponse});
+}
+
+class SearchBookErrorState extends SearchBookResultsState  {
+  final String message;
+
+  SearchBookErrorState({required this.message});
+}
