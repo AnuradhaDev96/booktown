bd7974e045100bfad385071ca5ee6ef91645b6ac
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-19 18:29:56 +0530

    B02 - Search books

--- lib/src/bloc/cubits/search_book_result_cubit.dart
+++ lib/src/bloc/cubits/search_book_result_cubit.dart
@@ -1,19 +1,34 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../models/view_models/search_book_result.dart';
 import '../../services/repositories/book_repository.dart';
 import '../states/search_book_results_state.dart';
 
 class SearchBookResultCubit extends Cubit<SearchBookResultsState> {
   SearchBookResultCubit() : super(LoadingSearchState());
+  SearchBookResult? searchResult;
 
+  /// Search books and update the state with pagination.
   void searchBook(String query, int page) {
     GetIt.instance<BookRepository>().searchBooksByTitle(query, page.toString()).then((result) {
       result.fold(
-            (newPage) {
-
-            },
-            (message) => emit(SearchBookErrorState(message: message)),
+        (pageResponse) {
+          // search term is same
+          if (searchResult != null && searchResult!.searchedTerm != query) {
+            searchResult!.setTotal(pageResponse.total);
+            searchResult!.updateCurrentTotal(pageResponse.total);
+            searchResult!.searchResults.addAll(pageResponse.books);
+          } else {
+            searchResult = SearchBookResult(
+              total: pageResponse.total,
+              currentPage: pageResponse.page,
+              searchResults: pageResponse.books,
+              searchedTerm: query,
+            );
+          }
+        },
+        (message) => emit(SearchBookErrorState(message: message)),
       );
     });
   }
