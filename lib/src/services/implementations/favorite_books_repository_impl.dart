de02eda3c9b0ee88b96a03c4af76717a04c9fb1e
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-20 15:42:17 +0530

    B04 - Manage favorites

---
+++ lib/src/services/implementations/favorite_books_repository_impl.dart
@@ -0,0 +1,36 @@
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../../models/dto/favorite_book_dto.dart';
+import '../repositories/favorite_books_repository.dart';
+import '../sqflite_client.dart';
+
+class FavoriteBooksRepositoryImpl implements FavoriteBooksRepository {
+  final _tableName = DbHelper.favoritesTable;
+  final _logger = Logger(printer: PrettyPrinter(methodCount: 0));
+
+  @override
+  Future<bool> addBookToFavoriteList(FavoriteBookDto instance) async {
+    final db = await GetIt.instance<SqlfliteClient>().getDatabase();
+
+    final result = await db.insert(_tableName, instance.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+
+    _logger.i("Sqflite INSERT into => $_tableName\nResult => $result");
+
+    return result == 0 ? false : true;
+  }
+
+  @override
+  Future<List<FavoriteBookDto>> getFavoriteBooks() async {
+    final db = await GetIt.instance<SqlfliteClient>().getDatabase();
+    final records = await db.query(_tableName);
+    _logger.i("Sqflite QUERY => $_tableName\nResult => $records");
+
+    List<FavoriteBookDto> list = records.map((e) => FavoriteBookDto.fromMap(e)).toList();
+
+    list.sort((a, b) => b.viewedAt.compareTo(a.viewedAt));
+
+    return list;
+  }
+}
