76de3216c282efde8bdef6ba98fa00fdc9491b33
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-20 15:24:02 +0530

    B04 - Manage favorites

--- lib/dependency_injector.dart
+++ lib/dependency_injector.dart
@@ -4,12 +4,17 @@ import 'src/services/dio_client.dart';
 import 'src/services/implementations/book_repository_impl.dart';
 import 'src/services/local_store.dart';
 import 'src/services/repositories/book_repository.dart';
+import 'src/services/sqflite_client.dart';
 
 abstract class DependencyInjector {
   /// Inject dependencies with get_it
   static void injectDependencies() {
+    // Data resources
     GetIt.instance.registerLazySingleton<LocalStore>(() => LocalStore());
     GetIt.instance.registerLazySingleton<DioClient>(() => DioClient());
+    GetIt.instance.registerLazySingleton<SqlfliteClient>(() => SqlfliteClient());
+
+    // Services
     GetIt.instance.registerLazySingleton<BookRepository>(() => BookRepositoryImpl());
   }
 }
