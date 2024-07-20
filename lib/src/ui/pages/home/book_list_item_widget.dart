9f25ee41ac3e76f8ba90b36a34f67d9e73b6d98c
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-20 12:06:08 +0530

    B04 - Book details

--- lib/src/ui/pages/home/book_list_item_widget.dart
+++ lib/src/ui/pages/home/book_list_item_widget.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 
+import '../../../bloc/cubits/fetch_book_details_cubit.dart';
+import '../../../bloc/states/fetch_book_details_state.dart';
+import '../../../config/alert_utils.dart';
 import '../../../config/app_routes.dart';
 import '../../../config/widget_keys.dart';
 import '../../../models/dto/book.dart';
 
 class BookListItemWidget extends StatelessWidget {
-  const BookListItemWidget({super.key, required this.bookDto});
+  BookListItemWidget({super.key, required this.bookDto});
 
   final BookDto bookDto;
+  final _fetchDetailsCubit = FetchBookDetailsCubit();
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: () {
-        context.loaderOverlay.show();
-        // WidgetKeys.mainNavKey.currentState!.pushNamed(RouteNames.bookDetailsPage);
-        context.loaderOverlay.hide();
-      },
-      child: SizedBox(height: 140, child: Text(bookDto.title)),
+    return BlocProvider<FetchBookDetailsCubit>(
+      create: (context) => _fetchDetailsCubit,
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          _fetchDetailsCubit.fetchBookDetails(bookDto.isbn13);
+          context.loaderOverlay.show();
+        },
+        child: BlocListener<FetchBookDetailsCubit, BookDetailsState>(
+          bloc: _fetchDetailsCubit,
+          listener: (BuildContext context, state) {
+            if (state is BookDetailsSuccessState) {
+              context.loaderOverlay.hide();
+              WidgetKeys.mainNavKey.currentState!.pushNamed(RouteNames.bookDetailsPage);
+            } else if (state is BookDetailsErrorState) {
+              context.loaderOverlay.hide();
+              AlertUtils.showSnackBar(state.errorMessage, AlertTypes.error);
+            }
+          },
+          child: SizedBox(height: 140, child: Text(bookDto.title)),
+        ),
+      ),
     );
   }
 }
