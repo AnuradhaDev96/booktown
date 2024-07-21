292709b264ce824eaa0c364cc915d14fc8c74fff
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-21 11:21:33 +0530

    B06 - Apply theme changes

--- lib/src/ui/pages/home/book_search_results_widget.dart
+++ lib/src/ui/pages/home/book_search_results_widget.dart
@@ -149,7 +149,7 @@ class SearchResultAppBar extends StatelessWidget {
                             TextSpan(
                               // text: '''"${searchResultCubit.loadedBooks?.searchedTerm}"''',
                               text:
-                                  '''"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."''',
+                                  '''"${searchResultCubit.loadedBooks?.searchedTerm}"''',
                               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                                     fontSize: 16.sp,
                                     letterSpacing: 0.4,
