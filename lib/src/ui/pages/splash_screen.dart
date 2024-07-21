f8e55329d9e241d668a0b8bfbc14f5d7d0ced7ab
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-21 12:48:26 +0530

    B07 - Apply animations

--- lib/src/ui/pages/splash_screen.dart
+++ lib/src/ui/pages/splash_screen.dart
@@ -36,25 +36,39 @@ class _SplashScreenState extends State<SplashScreen> {
         width: 100.w,
         height: 100.h,
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Color(0xFF0180C5),
-              Color(0xFF20B9CD),
-            ]
-          )
-        ),
-        child: Center(
-          child: Text(
-            "Booktown",
-            style: TextStyle(
-              letterSpacing: -2,
-              color: Colors.white,
-              fontSize: 29.sp,
-              fontWeight: FontWeight.w700,
+            gradient: LinearGradient(begin: Alignment.topRight, end: Alignment.bottomLeft, colors: [
+          Color(0xFF0180C5),
+          Color(0xFF20B9CD),
+        ])),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Center(
+              child: Text(
+                "Booktown",
+                style: TextStyle(
+                  letterSpacing: -2,
+                  color: Colors.white,
+                  fontSize: 29.sp,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              bottom: 10.h,
+              child: Text(
+                'IT, Programming\n& Computer Science Books',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white54,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16.sp,
+                  letterSpacing: -0.8,
+                  height: 1.2
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
