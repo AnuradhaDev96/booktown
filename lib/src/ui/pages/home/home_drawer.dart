35e4de2a8e9e81a796f69957c0c0231871d7dc56
AnuradhaDev96 <anusampath9470@gmail.com>
2024-07-21 15:21:04 +0530

    B07 - Apply animations

--- lib/src/ui/pages/home/home_drawer.dart
+++ lib/src/ui/pages/home/home_drawer.dart
@@ -3,6 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import '../../../bloc/cubits/toggle_theme_bloc.dart';
+import '../../../config/app_colors.dart';
 
 class HomeDrawer extends StatelessWidget {
   const HomeDrawer({super.key});
@@ -13,29 +14,92 @@ class HomeDrawer extends StatelessWidget {
       child: SafeArea(
         bottom: false,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 3.w, top: 3.h),
-                child: Text(
-                  'Change theme',
-                  style: Theme.of(context).textTheme.titleMedium,
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 3.w, top: 3.h),
+                    child: Text(
+                      'Change theme',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
                 ),
-              ),
+                StreamBuilder<ThemeMode>(
+                    stream: GetIt.instance<ToggleThemeBloc>().themeModeStream,
+                    builder: (context, snapshot) {
+                      return SwitchListTile(
+                        title: const Text("Dark mode"),
+                        value: (snapshot.data ?? ThemeMode.light) == ThemeMode.dark,
+                        onChanged: (value) {
+                          GetIt.instance<ToggleThemeBloc>().updateTheme(isDarkMode: value);
+                        },
+                      );
+                    }),
+                const Divider(),
+              ],
             ),
-            StreamBuilder<ThemeMode>(
-                stream: GetIt.instance<ToggleThemeBloc>().themeModeStream,
-                builder: (context, snapshot) {
-                  return SwitchListTile(
-                    title: const Text("Dark mode"),
-                    value: (snapshot.data ?? ThemeMode.light) == ThemeMode.dark,
-                    onChanged: (value) {
-                      GetIt.instance<ToggleThemeBloc>().updateTheme(isDarkMode: value);
-                    },
-                  );
-                }),
-            const Divider()
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 2.w, right: 2.w, bottom: 3.h),
+                    padding: EdgeInsets.symmetric(vertical: 1.h, horizontal: 1.5.w),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [AppColors.gradientBlue1, AppColors.gradientBlue2]),
+                      borderRadius: BorderRadius.circular(14.sp),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Booktown",
+                          style: TextStyle(
+                            letterSpacing: -2,
+                            color: Colors.white,
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 5, bottom: 3.h),
+                          child: Text(
+                            'IT, Programming & Computer Science Books',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                color: Colors.white60,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 15.sp,
+                                letterSpacing: -0.8,
+                                height: 1.2),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 4, bottom: 3.h),
+                          child: Text(
+                            'Platform to browse books covering trending subject in academia. Search your books and keep them safe as favorites.',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 13.sp,
+                              letterSpacing: -0.03,
+                              height: 1.1,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
